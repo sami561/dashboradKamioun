875337b60ad1193dc59c0cec21e0ca2102614148
sami ayachi <77441338+sami561@users.noreply.github.com>
2025-10-07 11:45:51 +0100

    Update authApi.js

--- src/state/authApi.js
+++ src/state/authApi.js
@@ -5,7 +5,7 @@ import { setToken, setUser } from "./authSlice";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://apigateway:3000",
+    baseUrl: "http://api-gateway:3000",
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       if (token) {
