11f29e5d8618b1dbecd5f811d289bbcf07ddad40
sami ayachi <77441338+sami561@users.noreply.github.com>
2025-10-07 11:39:00 +0100

    Update api.js

--- src/state/api.js
+++ src/state/api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000/",
+    baseUrl: "http://apigateway:3000",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("accessToken");
       if (token) {
